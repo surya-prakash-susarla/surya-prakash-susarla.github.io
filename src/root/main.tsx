00cdda6da0a577a8c4cdd84a229ed1b17720581c
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2022-08-12 13:35:04 -0400

    remove default code

---
+++ src/root/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+class MainComponent extends React.Component {
+
+  public render(): JSX.Element {
+    return <div> this is some sample text </div>
+  }
+}
+
+export default MainComponent;
