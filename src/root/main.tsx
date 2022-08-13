c175149eb4c96ca4143451d9dde248d9d26835e5
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2022-08-12 22:19:46 -0400

    minor updates

--- src/root/main.tsx
+++ src/root/main.tsx
@@ -1,48 +1,86 @@
 import './main.css';
 import { Layout, Menu, PageHeader, Typography } from 'antd';
-import React from 'react';
+import React, { useState } from 'react';
 import Card from 'antd/lib/card/Card';
 const { Footer, Sider } = Layout;
 
+enum PageSelection {
+  AboutMePage = 1,
+  ResumePage = 2,
+  ContactMePage = 3
+};
+
 const MainComponent: React.FC = () => {
+  const [pageSelection, setPageSelection] = useState(PageSelection.AboutMePage);
   return (
     <Layout style={{ minHeight: '100vh' }}>
       <Sider>
-        <SideMenu />
+        <SideMenu clickHandler={setPageSelection}/>
       </Sider>
       <Layout style={{ minHeight: '100vh' }}>
         <TitleBar />
-        <ContentPage />
+        <ContentPage pageSelection={pageSelection} />
         <FooterBar />
       </Layout>
     </Layout>
   );
 }
 
-const SideMenu: React.FC = () => {
+interface SideMenuProps {
+  clickHandler: (pageSelection: PageSelection) => void
+};
+
+const SideMenu: React.FC<SideMenuProps> = (props: SideMenuProps) => {
   return (
     <Menu defaultSelectedKeys={['1']}>
-      <Menu.Item key={'1'}> About Me </Menu.Item>
-      <Menu.Item key={'2'}> Resume </Menu.Item>
-      <Menu.Item key={'3'}> Contact </Menu.Item>
+      <Menu.Item key={'1'} onClick={() => { props.clickHandler(PageSelection.AboutMePage) }}> About Me </Menu.Item>
+      <Menu.Item key={'2'} onClick={() => { props.clickHandler(PageSelection.ResumePage) }}> Resume </Menu.Item>
+      <Menu.Item key={'3'} onClick={() => { props.clickHandler(PageSelection.ContactMePage) }}> Contact </Menu.Item>
     </Menu>
   );
 }
 
 const TitleBar: React.FC = () => {
   return (
-    <PageHeader>
-      <Typography.Title code={true} level={1} underline={true} >
+    <PageHeader className='header-text'>
+      <Typography.Title code={true} level={1} >
         Surya Prakash Susarla
       </Typography.Title>
     </PageHeader>
   )
 }
 
-const ContentPage: React.FC = () => {
+interface ContentPageProps {
+  pageSelection: PageSelection
+};
+
+const ContentPage: React.FC<ContentPageProps> = (props: ContentPageProps) => {
+  switch (props.pageSelection) {
+    case PageSelection.AboutMePage:
+      return <ContentCard title='About Me' content='This is about me.' />
+    case PageSelection.ResumePage:
+      return <ContentCard title='Resume' content='Resume page.' />
+    case PageSelection.ContactMePage:
+      return <ContentCard title='Contact Me' content='Contact me page.' />
+    default:
+      return null;
+  }
+}
+
+interface ContentCardProps {
+  title: string,
+  content: string
+};
+
+const ContentCard: React.FC<ContentCardProps> = (props: ContentCardProps) => {
   return (
     <Card>
-      This is some random card.
+      <Typography.Title code={true} underline={true} level={2} className='content-card-title'>
+        {props.title}
+      </Typography.Title>
+      <Typography.Paragraph code={true}>
+        {props.content}
+      </Typography.Paragraph>
     </Card>
   );
 }
@@ -61,7 +99,7 @@ const FooterBar: React.FC = () => {
   }, []);
 
   return (
-    <Footer>
+    <Footer className='footer-bar'>
       {time}
     </Footer>
   );
