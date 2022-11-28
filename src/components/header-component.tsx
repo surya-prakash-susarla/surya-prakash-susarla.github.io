62477159bc2b97fe279a1eeab65b5e856b424c9d
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2022-11-27 21:49:51 -0500

    add margin top to header

--- src/components/header-component.tsx
+++ src/components/header-component.tsx
@@ -6,7 +6,7 @@ export const HeaderBar: React.FC = () => {
   return (
     <React.Fragment>
       <Typography variant='h3' align={'center'}
-        sx={{ fontFamily: "'Source Code Pro', monospace" }}
+        sx={{ fontFamily: "'Source Code Pro', monospace", marginTop: "30px" }}
         gutterBottom>
         {kTitleBarText}
       </Typography>
